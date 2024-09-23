53addf80a9e3897518327042bf449cf5663533c3
SanuRaj-K <sanusrk29@gmail.com>
2024-09-23 16:09:39 +0530

    feat:pwa updated

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -71,7 +71,9 @@ function registerValidSW(swUrl, config) {
                 'New content is available and will be used when all ' +
                   'tabs for this page are closed. See https://cra.link/PWA.'
               );
-              window.location.reload(true)
+              window.location.reload();
+
+            
 
               // Execute callback
               if (config && config.onUpdate) {
