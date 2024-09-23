28f265cb785229a21f3d78a77ccec6df0c0425c9
SanuRaj-K <sanusrk29@gmail.com>
2024-09-23 15:53:55 +0530

    feat:styles added

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -57,7 +57,7 @@ function Input({ addedUsers, setAddedUsers }) {
       >
         <span> Testing the PWA</span>
         <div style={{}}>
-          <div style={{ display: "flex ", justifyItems: "center" }}>
+          <div style={{ display: "flex ", justifyItems: "center", marginTop:'12px' }}>
             <label htmlFor="name">Enter your Name:</label>
             <input
               type="text"
